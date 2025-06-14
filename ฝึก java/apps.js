27dbefba440ab8126caefa16b579714d0a292b31
Nattawwat1122 <inum27601@gmail.com>
2025-06-14 08:55:46 +0700

    Add files via upload

---
+++ ฝึก java/apps.js
@@ -0,0 +1,32 @@
+//document.write("<h1>ไอน้ำ</h1>");
+
+//alert("hi");
+
+//console.log("Hello")/*comment*/
+
+
+/*let num; //สร้้างตัวแปลไม่กำหนดค้า 
+num = "ไอน้ำครับ";
+document.write(num)
+
+num = "ยืมตัวหน้อย";
+
+
+let monny = 200
+let pri = 20.99
+let chec = false
+let checn = true
+
+let age = parseInt('10')
+console.log(age)
+console.log(typeof(age))
+let age1 = parseFloat('10.3')
+console.log(age1)
+console.log(typeof(age1))
+let age2 = +('10.3')
+console.log(age1)
+console.log(typeof(age2))*/
+
+
+let num = Array(1,2,3,4,5,6)
+console.log(num[4])
---
+++ ฝึก java/es6.js
@@ -0,0 +1,45 @@
+//Arrow function
+
+/*fullname=(f,l)=>f+l
+
+console.log(fullname("d","D")); */
+
+
+
+//object//////////////////////////////////////////////////
+
+/*const age=34
+const add="ee"
+const namew="ww"
+
+const customer = {
+   // cname : "n",
+ //   age :age,
+    add,
+    Showdata(){
+        console.log("=ชื่อ = ",namew )
+    }
+}
+
+customer.Showdata()*/ 
+///////////////////////////////////////////////////////
+/*let nname="สมชาย"
+
+const add=`ชื่อ = ${nname}
+ที่อยู่ 
+เบอร์ `
+
+console.log(add)*/
+
+//Spread operator/////////////////
+
+/*const newarr= [100,200,300,400]
+const data=[10,20,30,...newarr]
+
+const numnew=[33,45,77]
+
+data.push(...numnew)
+console.log(data);*/
+
+//rest parametor/////////////////////////////////////////////////////////////
+
